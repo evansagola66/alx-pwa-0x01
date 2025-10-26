fad00405a5a07f54b3354b989ef39ac13b954a5a
bitevans <evansagola6@gmail.com>
2025-10-26 19:01:12 +0000

    all

---
+++ alx-movie-app/interfaces/index.ts
@@ -0,0 +1,45 @@
+
+
+export default Movies;
+
+Expand the interfaces to accommodate for the Movies:
+
+Replace the content of interfaces/index.ts with the following:
+
+
+import { ReactNode } from "react";
+
+export interface ComponentProps {
+  children: ReactNode
+}
+
+export interface ButtonProps {
+  title: string
+  action?: () => void
+}
+
+export interface MovieProps {
+  id?: string
+  posterImage: string
+  releaseYear: string
+  title: string
+}
+
+interface PrimaryImage {
+  url: string
+}
+
+interface TitleText {
+  text: string
+}
+
+interface ReleaseYear {
+  year: string
+}
+
+export interface MoviesProps {
+  id: string
+  primaryImage: PrimaryImage
+  titleText: TitleText
+  releaseYear: ReleaseYear
+}
